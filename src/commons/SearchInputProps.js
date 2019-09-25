c6d39a0f38bf18bdb029e1087d39672c764dec79
Ricardo Cerqueira <raxs64@gmail.com>
2019-09-25 16:09:39 -0300

    search with props adv

--- src/commons/SearchInputProps.js
+++ src/commons/SearchInputProps.js
@@ -1,69 +1,82 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import { withStyles } from '@material-ui/styles';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles } from "@material-ui/styles";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
 const styles = {
   root: {
-    width: '250px',
-    height: '35px',
-    display: 'flex',
-    alignItems: 'center',
-    borderRadius: '18px',
-    border: 'solid 0.5px #6d6d6d',
-    boxShadow: 'none',
-    boxSizing: 'border-box',
+    width: "250px",
+    height: "35px",
+    display: "flex",
+    alignItems: "center",
+    borderRadius: "18px",
+    border: "solid 0.5px #6d6d6d",
+    boxShadow: "none",
+    boxSizing: "border-box",
     "&:hover": {
       border: "solid 2px #6d6d6d",
       "& iconButton": {
-        backgroundColor: 'green',
-        padding: 4,
+        color: "#00c85f",
+        padding: 4
+      }
+    },
+    "&:focus-within": {
+      border: "solid 2px #00c85f",
+      "~ iconButton": {
+        color: "red"
       }
     }
   },
   input: {
-    marginLeft: '23px',
+    marginLeft: "23px",
     flex: 1,
+    "&:hover ~ IconButton":{
+      color:'red',
+   },
   },
   iconButton: {
-    marginRight: '0.5px',
-    padding: 5,
-  },
+    marginRight: "1px",
+    padding: 4,
+    "&:focus-within": {
+      color: "white",
+      backgroundColor: '#00c85f',
+    }
+  }
 };
 
-function SearchInputProps(props) {
+function hello() {
+  console.log("aaa");
+}
 
-  const {
-    classes,
-    disabled,
-    placeholder
-  } = props;
+function SearchInputProps(props) {
+  const { classes, disabled, placeholder } = props;
+
   return (
     <Paper className={classes.root}>
       <InputBase
         className={classes.input}
-        placeholder={placeholder}
-        inputProps={{ 'aria-label': 'search google maps' }}
+        placeholder="Buscar"
+        inputProps={{ "aria-label": "search google maps" }}
         disabled={disabled}
+
       />
-      <IconButton className={classes.iconButton} aria-label="search" disabled={disabled}>
+      <IconButton
+        className={classes.iconButton}
+        aria-label="search"
+        disabled={disabled}
+        onClick={hello}
+      >
         <SearchIcon />
       </IconButton>
-
     </Paper>
-  )
+  );
 }
 
 SearchInputProps.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SearchInputProps);
-
-
-
-
